d7f5f70b2349f613f951843664d0e0612dd97f4b
Hubert Kasprzycki <hubertkasprzycki6@gmail.com>
2024-02-07 15:18:22 +0100

    Implement sand drawing

--- Sand.py
+++ Sand.py
@@ -1,77 +1,69 @@
 import math
 import random
-from tkinter import Canvas, Event
 
 
 class Sand:
-    GRAIN_SIZE = 10
-    grains: dict[tuple[int, int], int] = {}
+    DEFAULT_GRAIN_SIZE = 10
+    SAND_COLORS = ["#ffae00", "#ffb619", "#ffbc2b", "#ffc240"]
+    _grains: dict[tuple[int, int], str] = {}
 
-    def __init__(self, canvas: Canvas, width: int, height: int) -> None:
-        self.canvas = canvas
-        self.width, self.height = self.get_grid_position(width, height)
+    def __init__(
+        self,
+        width: int,
+        height: int,
+        grain_size: int = DEFAULT_GRAIN_SIZE,
+    ) -> None:
+        self._grain_size = grain_size
+        self.width, self.height = self._get_grid_position(width, height)
 
-        canvas.bind("<Button-1>", self.add_grains)
-        canvas.bind("<B1-Motion>", self.add_grains)
-
-    def add_grains(self, event: Event) -> None:
-        for r in range(0, self.GRAIN_SIZE * 4, self.GRAIN_SIZE):
-            for deg10 in range(0, int(2 * math.pi * 5)):
-                deg = deg10 / 5
+    def add_grains(self, x: int, y: int) -> None:
+        for r in range(0, self._grain_size * 3, self._grain_size):
+            for deg in range(0, int(2 * math.pi * 5)):
+                deg = deg / 5
                 dx, dy = r * math.cos(deg), r * math.sin(deg)
-                if random.random() < 0.5:
-                    self.add_single_grain(event.x + dx, event.y + dy)
+                if random.random() < 0.4:
+                    self.add_single_grain(int(x + dx), int(y + dy))
 
-    def add_single_grain(self, x: float, y: float) -> None:
-        grid_x, grid_y = self.get_grid_position(x, y)
+    def add_single_grain(self, x: int, y: int) -> None:
+        grid_x, grid_y = self._get_grid_position(x, y)
 
-        if (grid_x, grid_y) in self.grains:
-            return
-
-        x = grid_x * self.GRAIN_SIZE
-        y = grid_y * self.GRAIN_SIZE
-
-        color = random.choice(["#ffae00", "#ffb619", "#ffbc2b", "#ffc240"])
-        grain = self.canvas.create_rectangle(
-            x,
-            y,
-            x + self.GRAIN_SIZE,
-            y + self.GRAIN_SIZE,
-            fill=color,
-            outline=color,
-        )
-
-        self.grains[grid_x, grid_y] = grain
+        if (grid_x, grid_y) not in self._grains:
+            self._grains[grid_x, grid_y] = random.choice(self.SAND_COLORS)
 
     def update(self) -> None:
-        coords = sorted(self.grains.keys(), key=lambda p: p[::-1], reverse=True)
+        coords = sorted(self._grains.keys(), key=lambda p: p[::-1], reverse=True)
 
         for x, y in coords:
-            if self.can_move_to(x, y + 1):
-                self.move_grain(x, y, 0, 1)
-            elif self.can_move_to(x + 1, y + 1):
-                self.move_grain(x, y, 1, 1)
-            elif self.can_move_to(x - 1, y + 1):
-                self.move_grain(x, y, -1, 1)
+            if self._can_move_to(x, y + 1):
+                self._move_grain(x, y, 0, 1)
+            elif self._can_move_to(x + 1, y + 1):
+                self._move_grain(x, y, 1, 1)
+            elif self._can_move_to(x - 1, y + 1):
+                self._move_grain(x, y, -1, 1)
 
-    def can_move_to(self, x, y):
+    def _can_move_to(self, x, y):
         return (
-            (x, y) not in self.grains and 0 <= x < self.width and 0 <= y < self.height
+            (x, y) not in self._grains and 0 <= x < self.width and 0 <= y < self.height
         )
 
-    def move_grain(self, x: int, y: int, dx: int, dy: int) -> None:
+    def _move_grain(self, x: int, y: int, dx: int, dy: int) -> None:
         target_x, target_y = x + dx, y + dy
+        self._grains[target_x, target_y] = self._grains.pop((x, y))
 
-        grain = self.grains.pop((x, y))
-        self.grains[target_x, target_y] = grain
-
-        self.canvas.move(grain, dx * self.GRAIN_SIZE, dy * self.GRAIN_SIZE)
-
-    def get_grain_position(self, grain: int) -> tuple[int, int]:
-        return self.get_grid_position(*self.canvas.coords(grain))
-
-    def get_grid_position(self, x: float, y: float) -> tuple[int, int]:
+    def _get_grid_position(self, x: int, y: int) -> tuple[int, int]:
         return (
-            math.floor(x / self.GRAIN_SIZE),
-            math.floor(y / self.GRAIN_SIZE),
+            math.floor(x / self._grain_size),
+            math.floor(y / self._grain_size),
         )
+
+    def get_grains(self):
+        return [
+            (self._get_real_position(*pos), color)
+            for pos, color in self._grains.items()
+        ]
+
+    def _get_real_position(self, grid_x: int, grid_y: int) -> tuple[int, int]:
+        return grid_x * self._grain_size, grid_y * self._grain_size
+
+    def get_grain_size(self) -> int:
+        return self._grain_size
--- main.py
+++ main.py
@@ -1,29 +1,46 @@
-import pygame
+import pygame as pg
+
+from Sand import Sand
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+
+def draw_sand(screen: pg.Surface, sand: Sand) -> None:
+    for pos, color in sand.get_grains():
+        rect = pg.Rect(pos, (sand.get_grain_size(), sand.get_grain_size()))
+        pg.draw.rect(screen, color, rect)
 
 
 def main() -> None:
-    # pygame setup
-    pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
-    clock = pygame.time.Clock()
+    # pg setup
+    pg.init()
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pg.time.Clock()
     running = True
-    dt = 0
+
+    sand = Sand(SCREEN_WIDTH, SCREEN_HEIGHT)
 
     # main loop
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 running = False
 
         screen.fill("white")
 
-        # TODO logic of sand
+        sand.update()
 
-        pygame.display.flip()
+        mouse_pressed, _, _ = pg.mouse.get_pressed()
+        if mouse_pressed:
+            sand.add_grains(*pg.mouse.get_pos())
 
-        dt = clock.tick(60) / 1000
+        draw_sand(screen, sand)
 
-    pygame.quit()
+        pg.display.flip()
+        clock.tick(60)
+
+    pg.quit()
 
 
 if __name__ == "__main__":
