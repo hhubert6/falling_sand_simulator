de1e7df79800e87935fd44ffc33d69bb16f57909
Hubert Kasprzycki <hubertkasprzycki6@gmail.com>
2024-02-07 14:30:40 +0100

    Replace App tkinter class with main function
    main function consists of pygame setup and running the main loop

--- main.py
+++ main.py
@@ -1,37 +1,30 @@
-import tkinter as tk
-
-from Sand import Sand
+import pygame
 
 
-class App(tk.Tk):
-    WINDOW_HEIGHT = 700
-    WINDOW_WIDTH = 1000
-    REFRESH_RATE = 0.01
+def main() -> None:
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
 
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.geometry(f"{self.WINDOW_WIDTH}x{self.WINDOW_HEIGHT}")
+    # main loop
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        self.canvas = self.create_canvas()
-        self.sand = Sand(self.canvas, self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
+        screen.fill("white")
 
-    def create_canvas(self) -> tk.Canvas:
-        canvas = tk.Canvas(
-            self,
-            bg="white",
-            width=self.WINDOW_WIDTH,
-            height=self.WINDOW_HEIGHT,
-        )
-        canvas.pack()
-        return canvas
+        # TODO logic of sand
 
-    def loop(self) -> None:
-        while True:
-            self.sand.update()
-            self.update_idletasks()
-            self.update()
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
-    app = App()
-    app.loop()
+    main()
